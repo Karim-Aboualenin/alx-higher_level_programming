785924a00c7d3739a91cc49efa4ce313a1832673
Karim-Aboualenin <karim72727272@yahoo.com>
2023-10-31 20:57:15 +0200

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ""
+    for c in (str):
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            string += chr(ord(c) - 32)
+        else:
+            string += c
+    print(f"{string}")
