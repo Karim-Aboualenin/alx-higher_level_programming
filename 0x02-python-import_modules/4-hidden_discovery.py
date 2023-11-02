c1db7edad57a6cf4dfab3bd87c49592ed2b4d549
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-02 20:56:32 +0200

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
-            print("{:s}".format(name))
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{:s}".format(i))
