2f5750754ba8ea16029b05f71e116fb17164a12c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-02 20:43:14 +0200

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     argc = len(sys.argv)
     result = 0
     if argc == 1:
-       result = 0
-    else:   
+        result = 0
+    else:
         for i in range(1, argc):
             result += int(sys.argv[i])
     print("{}".format(result))
-        
+    
