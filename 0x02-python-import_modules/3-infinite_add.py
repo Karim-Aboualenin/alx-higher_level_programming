f2730bf3430b806274fae6a5c025eeae25635c69
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-02 20:41:28 +0200

    3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    result = 0
+    if argc == 1:
+       result = 0
+    else:   
+        for i in range(1, argc):
+            result += int(sys.argv[i])
+    print("{}".format(result))
+        
