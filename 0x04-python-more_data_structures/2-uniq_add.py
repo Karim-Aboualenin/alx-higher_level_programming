55943fd27745c138a0d01ae9eaca5ab2976ffc55
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-08 20:06:54 +0200

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list =[]
+    sum = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in new_list:
+            sum += my_list[i]
+            new_list.append(my_list[i])
+    return sum
