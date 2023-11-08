6f3609252fed444422f4c25a7c76317383a2bd7e
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-08 19:52:44 +0200

    1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+def search_replace(my_list, search, replace):
+    mat = my_list.copy()
+    for i in range(len(my_list)):
+       if my_list[i] == search:
+           mat[i] = replace
+    return mat
